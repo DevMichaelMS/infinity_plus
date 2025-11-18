6d3e6dbe3e05e7ea8889342fc7c1e109d5fa9493
Michael <michael.ms.est@gmail.com>
2025-11-18 00:51:46 -0300

    criando a FAQ

--- src/scripts.js
+++ src/scripts.js
@@ -1,28 +1,39 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const buttons = document.querySelectorAll('[data-tab-button]');
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll("[data-tab-button]");
+  const questions = document.querySelectorAll("[data-faq-question]");
 
-    buttons.forEach(function (button) {
-        button.addEventListener('click', function (event) {
-            const abaAlvo = event.target.dataset.tabButton;
-            const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`);
-            escondeAbas();
-            aba.classList.add('shows__list--is-active');
-            ativaBotao(event.target);
-        });
+  const ativaBotao = (botaoAtivo) => {
+    const botoes = document.querySelectorAll("[data-tab-button]");
+    botoes.forEach((botao) => {
+      botao.classList.remove("shows__tabs__button--is-active");
     });
+    botaoAtivo.classList.add("shows__tabs__button--is-active");
+  };
+
+  const escondeAbas = () => {
+    const abas = document.querySelectorAll("[data-tab-id]");
+    abas.forEach((aba) => {
+      aba.classList.remove("shows__list--is-active");
+    });
+  };
+
+  const abreOuFechaResposta = (elemento) => {
+    const classe = "faq__questions__item--is-open";
+    const elementoPai = elemento.currentTarget.parentNode;
+
+    elementoPai.classList.toggle(classe);
+  };
+  buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const abaAlvo = event.target.dataset.tabButton;
+      const aba = document.querySelector(`[data-tab-id="${abaAlvo}"]`);
+      escondeAbas();
+      aba.classList.add("shows__list--is-active");
+      ativaBotao(event.target);
+    });
+  });
+
+  questions.forEach((question) => {
+    question.addEventListener("click", abreOuFechaResposta);
+  });
 });
-
-function escondeAbas() {
-    const abas = document.querySelectorAll('[data-tab-id]');
-    abas.forEach(function (aba) {
-        aba.classList.remove('shows__list--is-active');
-    });
-}
-
-function ativaBotao(botaoAtivo) {
-    const botoes = document.querySelectorAll('[data-tab-button]');
-    botoes.forEach(function (botao) {
-        botao.classList.remove('shows__tabs__button--is-active');
-    });
-    botaoAtivo.classList.add('shows__tabs__button--is-active');
-}
